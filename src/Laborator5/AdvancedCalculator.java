f6127108958e7e26a00f803e0ea3f292a3690cf5
Antonio1128 <antonio.popelca2811@gmail.com>
2026-03-31 09:18:09 +0300

    Laborator 5

---
+++ src/Laborator5/AdvancedCalculator.java
@@ -0,0 +1,43 @@
+package Laborator5;
+import java.lang.Math;
+
+import static java.lang.Math.pow;
+
+public class AdvancedCalculator extends IntCalculator {
+
+    public AdvancedCalculator(int state)
+    {
+        super(state);
+    }
+
+    public AdvancedCalculator didvide(int value)
+    {
+        if(value!=0)
+        {
+            setState(getState()/value);
+            return this;
+        }
+
+        else
+        {
+            System.out.println("error/0");
+            return this;
+        }
+
+
+    }
+    public AdvancedCalculator power(int value) {
+        setState((int) pow(getState(), value));
+        return this;
+    }
+    public AdvancedCalculator nthRoot(int n) {
+        if (getState() < 0) {
+            System.out.println("eroare <0");
+            return this;
+        }
+        setState((int) pow(getState(), 1.0 / n));
+        return this;
+    }
+}
+
+
---
+++ src/Laborator5/AppLab5.java
@@ -0,0 +1,12 @@
+package Laborator5;
+
+public class AppLab5 {
+
+    public static void main(String[] args)
+    {
+        IntCalculator calculator = new IntCalculator(10);
+        int result = calculator.add(5).subtract(3).multiply(2).result();
+        System.out.println("a) " + result);
+
+    }
+}
